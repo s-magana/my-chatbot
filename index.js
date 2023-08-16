4981b620f5adf043ae916e392858e0240093b04b
Steve Magana <steveamagana@gmail.com>
2023-08-16 16:40:03 -0700

    Store chat history and send with each request to have ongoing conversation with bot

--- index.js
+++ index.js
@@ -14,16 +14,22 @@ async function main() {
     console.log(colors.bold.green("Welcome to your personal chatbot!"));
     console.log(colors.bold.green("Start chatting with your bot."));
 
+    const chatHistory = [];
+
     while (true) {
-        const userInput = readlineSync.question(colors.brightBlue("You: "));
+        const userInput = readlineSync.question(colors.blue("You: "));
 
         try {
+            // Iterate over history to form messages
+            const messages = chatHistory.map(([role, content]) => ({ role, content }));
+
+            // Add most recent user input
+            messages.push({ role: "user", content: userInput });
+
             // Make request to API using user input 
             const chatCompletion = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
-                messages: [
-                    { role: "user", content: userInput }
-                ],
+                messages: messages,
                 temperature: 0,
             });
 
@@ -31,11 +37,15 @@ async function main() {
             const completionContent = chatCompletion.data.choices[0].message.content
 
             if (userInput.toLowerCase() === "exit") {
-                console.log(colors.magenta("Bot: ") + completionContent)
+                console.log(colors.magenta("Bot: ") + completionContent);
                 return; 
             }
 
-            console.log(colors.magenta("Bot: ") + completionContent)
+            console.log(colors.magenta("Bot: ") + completionContent);
+
+            // Update history with user input and assistant response
+            chatHistory.push(["user", userInput]);
+            chatHistory.push(["assistant", completionContent]);
         } catch (error) {
             console.error(colors.red(error));
         }
